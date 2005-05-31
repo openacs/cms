d6b320d738d87c4e09d24216e4d475a2bd8c4049
Michael Steigman <michael@steigman.net>
2005-05-31 14:46:58 +0000

    tidy up template-unregister

--- www/modules/items/template-unregister.tcl
+++ www/modules/items/template-unregister.tcl
@@ -1,20 +1,17 @@
-# Add a template of the item
+ad_page_contract {
 
-request create
-request set_param item_id -datatype integer
-request set_param template_id -datatype integer
-request set_param context -datatype text
+    @author Michael Steigman
+    @creation-date May 2005
 
-
-db_transaction {
-    if { [catch { db_exec_plsql template_unregister "begin
-         content_item.unregister_template(
-             template_id => :template_id, 
-             item_id     => :item_id, 
-             use_context => :context ); 
-         end;" } err_msg] } {
-        ns_log notice "template-remove.tcl got an error: $err_msg"
-    }
+} {
+    { item_id:naturalnum }
+    { template_id:naturalnum }
+    { context }
+    { mount_point:optional "sitemap" }
+    { item_props_tab:optional "publishing" }
 }
 
-template::forward "../items/index?item_id=$item_id&#templates"
+content::item::unregister_template -item_id $item_id \
+    -template_id $template_id -use_context $context
+
+ad_returnredirect [export_vars -base index {item_id mount_point item_props_tab}]
