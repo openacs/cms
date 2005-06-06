608a36ac7630bb36069f4dbe36433a7d66acb7ef
Michael Steigman <michael@steigman.net>
2005-06-06 15:34:55 +0000

    copy index.vuh to /www/subsite

--- tcl/install-procs.tcl
+++ tcl/install-procs.tcl
@@ -1,11 +1,25 @@
 ad_library {
     install callbacks
 }
+
 namespace eval cms::install {}
+
 ad_proc -public cms::install::package_instantiate { -package_id } {
     Procedures to run on package instantiation
 } {
+    # create modules for new instance
     cm::modules::install::create_modules -package_id $package_id
+
+    set subsite_dir "/www"
+    append subsite_dir [site_node::get_url_from_object_id -object_id [ad_conn subsite_id]]
+    # check that directory exists and...
+    if { ![file exists $subsite_dir] } {
+	file mkdir $subsite_dir
+    }
+
+    # copy content delivery .vuh file to subsite root
+    file copy -force /packages/cms/www/index.vuh $subsite_dir
+    
 }
 
 ad_proc -public cms::install::package_uninstantiate { -package_id } {
