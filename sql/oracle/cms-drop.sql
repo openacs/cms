47ea1217426352fef3c74865b9f3b7afe182afab
Michael Steigman <michael@steigman.net>
2005-06-07 20:54:42 +0000

    add 2 tables to (still deficient) drop scripts

--- sql/oracle/cms-drop.sql
+++ sql/oracle/cms-drop.sql
@@ -2,3 +2,5 @@ drop table cm_attribute_widget_params;
 drop table cm_attribute_widgets;
 drop table cm_form_widget_params;
 drop table cm_form_widgets;
+drop table subsite_package_map;
+crop table cm_modules;
