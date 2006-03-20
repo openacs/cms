0d815d18df71885c821b825bdb985d301b3b821e
Michael Steigman <michael@steigman.net>
2006-03-20 20:30:41 +0000

    utilize CR API; add link to preview item

--- www/modules/items/item-header.adp
+++ www/modules/items/item-header.adp
@@ -1,13 +1,13 @@
 <nobr>
 <p>
 <include src="../../bookmark" mount_point="@mount_point@" id="@item_id@">
-@page_title;noquote@ 
+@page_title;noquote@ (<a href="@path@" target="_new">preview</a>)
 </p>
 </nobr>
 <p/>
 
 &nbsp;&nbsp;&nbsp;
-<if @description@ not nil>@description;noquote@</if>
+<if @content_item.description@ not nil>@content_item.description;noquote@</if>
 <else>No description</else>
 
 <p/>
--- www/modules/items/item-header.tcl
+++ www/modules/items/item-header.tcl
@@ -2,8 +2,6 @@ request create
 request set_param item_id -datatype integer
 request set_param mount_point -datatype keyword -optional -value sitemap
 
-# query the content_type of the item ID so we can check for a custom info page
-db_1row get_info "" -column_array info
-template::util::array_to_vars info
-
-set page_title "Content Item - $title"
+content::item::get -item_id $item_id -revision latest -array_name content_item
+set path "[subsite::get_element -subsite_id [ad_conn subsite_id] -element url -notrailing][content::item::get_path -item_id $item_id]"
+set page_title "Content Item - $content_item(title)"
