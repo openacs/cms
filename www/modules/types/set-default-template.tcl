4d0de1584743a25d112ce3a47624b7c8dc91cae7
Michael Steigman <michael@steigman.net>
2006-11-20 21:56:05 +0000

    a little more cleanup and use of the cr api

--- www/modules/types/set-default-template.tcl
+++ www/modules/types/set-default-template.tcl
@@ -1,29 +1,14 @@
 # /cms/www/modules/types/set-default-template.tcl
 # Sets a template registered to a content type/context to be the default
 
-
 request create
 request set_param template_id  -datatype integer
 request set_param context      -datatype keyword
 request set_param content_type -datatype keyword
-request set_param return_url   -datatype text     -optional
+request set_param type_props_tab -datatype text -optional -value templates
+request set_param mount_point -datatype text -optional -value types
 
-db_transaction {
+content::type::set_default_template -content_type $content_type \
+    -template_id $template_id $context
 
-
-# set the default template, automatically unsetting any preexisting default
-db_exec_plsql set_default_template "
-  begin
-  content_type.set_default_template(
-      template_id  => :template_id,
-      content_type => :content_type,
-      use_context  => :context );
-  end;"
-}
-
-# set the default return_url if none exists
-if { [template::util::is_nil return_url] } {
-    set return_url "index?content_type=$content_type&mount_point=types"
-}
-
-forward $return_url
+ad_returnredirect [export_vars -base index {content_type mount_point type_props_tab}]
--- www/modules/types/unregister-template.tcl
+++ www/modules/types/unregister-template.tcl
@@ -1,7 +1,6 @@
 # modules/types/unregister-template.tcl
 #  unregisters a template
 
-
 request create
 request set_param template_id -datatype integer
 request set_param context -datatype keyword
@@ -9,8 +8,7 @@ request set_param content_type -datatype keyword
 request set_param type_props_tab -datatype text -optional -value templates
 request set_param mount_point -datatype text -optional -value types
 
-db_transaction {
-    db_exec_plsql unregister_template {}
-}
+content::type::unregister_template -content_type $content_type \
+    -template_id $template_id -use_context $context
 
 ad_returnredirect [export_vars -base index {content_type mount_point type_props_tab}]
